42b4848dbdcf23ad44087bdf43b9c0607ead8303
afrahaamer <30327502+afrahaamer@users.noreply.github.com>
2021-02-07 15:08:57 +0530

    updated test.py

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import time
 import datetime
 print("Current Time: ", datetime.datetime.now())
+print("Current Year: ", datetime.date.today().strftime("%Y"))
+print("Current Month: ", datetime.date.today().strftime("%B"))
+print("Current Week: ", datetime.date.today().strftime("%W"))
+print("Current Weekend: ", datetime.date.today().strftime("%w"))
+print("Current Day of the Month: ", datetime.date.today().strftime("%j"))
+print("Current Day of the Week: ", datetime.date.today().strftime("%A"))
