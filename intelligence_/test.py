787c0e3dbbcccbd67638b095bd797e315caa5cd6
Bilal Aamer <bilalahmedaamer@gmail.com>
2021-02-21 19:57:14 +0530

    added test.py and test2.py

--- intelligence_/test.py
+++ intelligence_/test.py
@@ -1,6 +1,22 @@
-import blooms_d 
+class student:
+    def __init__(self, name = str(), ocean = [0,0,0,0,0], riasec = [0,0,0,0,0,0], chosen_subjects = []):
+        self.name = name
+        self.ocean = ocean
+        self.riasec = riasec
+        self.chosen_subjects = chosen_subjects
 
-blooms_d.choice = 1
-blooms_d.file_name = 'D:/3PAI/intelligence_/resources/QP.pdf'
+class subject:
+    def __init__(self, nameosub = str(), cei = 0, blooms = ()):
+        self.nameosub = nameosub
+        self.cei = cei
+        self.blooms = blooms
 
-print(blooms_d.blooms(blooms_d.choice,blooms_d.file_name))
+s1 = subject(nameosub="Math")
+s2 = subject(nameosub="Chem")
+s3 = subject(nameosub="Phy")
+s4 = subject(nameosub="Bio")
+
+S1 = student(name= "Bilal Aamer", chosen_subjects=[s1, s2,s3,s4])
+
+
+print(S1.chosen_subjects[0].nameosub, S1.name)
---
+++ interest_/test2.py
@@ -0,0 +1,32 @@
+def ocean_calc():
+    ocean = []
+    for i in range(10):
+        ocean.append(int(input("Enter " + str(i+1) + " input: "))) 
+
+'''
+The Ten Item Personality Inventory was administered (see Gosling, S. D., Rentfrow, P. J., & Swann, W. B., Jr. (2003). A Very Brief Measure of the Big Five Personality Domains. Journal of Research in Personality, 37, 504-528.):
+
+TIPI1	Extraverted, enthusiastic.
+TIPI2	Critical, quarrelsome.
+TIPI3	Dependable, self-disciplined.
+TIPI4	Anxious, easily upset.
+TIPI5	Open to new experiences, complex.
+TIPI6	Reserved, quiet.
+TIPI7	Sympathetic, warm.
+TIPI8	Disorganized, careless.
+TIPI9	Calm, emotionally stable.
+TIPI10	Conventional, uncreative.
+
+The TIPI items were rated "I see myself as:" _____ such that
+
+1 = Disagree strongly
+2 = Disagree moderately
+3 = Disagree a little
+4 = Neither agree nor disagree
+5 = Agree a little
+6 = Agree moderately
+7 = Agree strongly
+
+'''
+
+ocean_calc()
